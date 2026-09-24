19db88c70057737fd60ecdc0a5abf85337667d6a
Madhu-hub23 <p25ds010>
2026-09-24 09:37:12 +0530

    Fix cloud MySQL connection

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "P25DS010",
-    database: "smart_expense_tracker",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
